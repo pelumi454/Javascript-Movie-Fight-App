8de4233f4433615adda7204d5288b113ad7557c7
pelumi454 <amoochristianah454@gmail.com>
2022-08-19 09:58:29 +0100

    fix: try example of a get request using axios

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const fetchData = async () => {
-    const response = await axios.get('', {
-        params: {
-            apikey: 'd9835cc5',
-            s: 'avangers'
-        }
-    })
+    const response = await axios.get("http://www.omdbapi.com/", {
+      params: {
+        apikey: "a29c9a18",
+        s: "avengers",
+      },
+    });
     console.log(response.data)
 }
 fetchData();
