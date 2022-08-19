af5405fe273266813941b65fc569b9516825cddc
pelumi454 <amoochristianah454@gmail.com>
2022-08-19 13:05:25 +0100

    fix: handling error in the fetchData function

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const fetchData = async (searchTerm) => {
       s: searchTerm,
     },
   });
-  console.log(response.data);
+  if (response.data.Error) {
+    return [];
+  }
+  return response.data.Search;
 };
 
 const input = document.querySelector("input");
@@ -22,7 +25,14 @@ const debounce = (func, delay = 1000) => {
   };
 };
 
-const onInput = (event) => {
-  fetchData(event.target.value);
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value);
+  for (let movie of movies) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <img src="${movie.Poster}" />
+      <h1>${movie.Title}</h1>`;
+    document.querySelector("#target").appendChild(div);
+  }
 };
 input.addEventListener("input", debounce(onInput, 500));
