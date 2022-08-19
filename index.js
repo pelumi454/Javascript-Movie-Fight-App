66d365055376211065217a235412b5fe34ec1ce3
pelumi454 <amoochristianah454@gmail.com>
2022-08-19 13:12:46 +0100

    fix: add menu dropdown button to index.html

--- index.js
+++ index.js
@@ -1,38 +1,32 @@
-const fetchData = async (searchTerm) => {
-  const response = await axios.get("http://www.omdbapi.com/", {
+const fetchData = async searchTerm => {
+  const response = await axios.get('http://www.omdbapi.com/', {
     params: {
-      apikey: "a29c9a18",
-      s: searchTerm,
-    },
+      apikey: 'd9835cc5',
+      s: searchTerm
+    }
   });
+
   if (response.data.Error) {
     return [];
   }
+
   return response.data.Search;
 };
 
-const input = document.querySelector("input");
+const input = document.querySelector('input');
 
-const debounce = (func, delay = 1000) => {
-  let timeoutId;
-  return (...args) => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-      func.apply(null, args);
-    }, delay);
-  };
-};
-
-const onInput = async (event) => {
+const onInput = async event => {
   const movies = await fetchData(event.target.value);
+
   for (let movie of movies) {
-    const div = document.createElement("div");
+    const div = document.createElement('div');
+
     div.innerHTML = `
       <img src="${movie.Poster}" />
-      <h1>${movie.Title}</h1>`;
-    document.querySelector("#target").appendChild(div);
+      <h1>${movie.Title}</h1>
+    `;
+
+    document.querySelector('#target').appendChild(div);
   }
 };
-input.addEventListener("input", debounce(onInput, 500));
+input.addEventListener('input', debounce(onInput, 500));
